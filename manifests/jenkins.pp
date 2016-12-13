8f4a6b7020987e83fd42b0a33bb974c1715a8a75
neilmillard <neil@neilmillard.com>
2016-12-13 13:51:29 +0000

    required plugins

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -3,6 +3,24 @@ class profile::jenkins () {
   jenkins::plugin { 'git':
     version => '3.0.1'
   }
+  jenkins::plugin { 'workflow-scm-step':
+    version => '1.14.2'
+  }
+  jenkins::plugin { 'git-client':
+    version => '2.1.0'
+  }
+  jenkins::plugin { 'mailer':
+    version => '1.18'
+  }
+  jenkins::plugin { 'matrix-project':
+    version => '1.7.1'
+  }
+  jenkins::plugin { 'scm-api':
+    version => '1.3'
+  }
+  jenkins::plugin { 'ssh-credentials':
+    version => '1.12'
+  }
   # jenkins::job { 'test-build-job':
   #   config => template("${templates}/test-build-job.xml.erb"),
   # }
