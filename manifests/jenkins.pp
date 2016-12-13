ceb4c960ce7f1c6649e9f2969860a95271114f7b
neilmillard <neil@neilmillard.com>
2016-12-13 15:23:14 +0000

    add a job,
    this need credentials that don't exist in config yet

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -1,6 +1,9 @@
 class profile::jenkins () {
   include jenkins
 
+  jenkins::job { 'build%20webserver':
+    config => template("${module_name}/jenkins/build_webserver.xml.erb")
+  }
   # jenkins::job { 'test-build-job':
   #   config => template("${templates}/test-build-job.xml.erb"),
   # }
